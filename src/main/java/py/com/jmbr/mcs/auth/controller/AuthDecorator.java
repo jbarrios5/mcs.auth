ec9632edffa2b76659d5ef377da45ef5935d7594
juan <marcecinco@fpuna.edu.py>
2023-12-01 17:31:17 -0300

    Se agrega validacion del accesstoken

--- src/main/java/py/com/jmbr/mcs/auth/controller/AuthDecorator.java
+++ src/main/java/py/com/jmbr/mcs/auth/controller/AuthDecorator.java
@@ -2,10 +2,7 @@ package py.com.jmbr.mcs.auth.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import py.com.jmbr.java.commons.beans.mcs.auth.AuthPostReqData;
 import py.com.jmbr.java.commons.beans.mcs.auth.AuthPostResData;
@@ -25,5 +22,11 @@ public class AuthDecorator {
     @Operation(summary = "authenticate user",description = "Authenticate user by password and document")
     public AuthPostResData login(@RequestBody @Valid AuthPostReqData req) throws JMBRException {
         return authService.login(req.getData());
+    }
+
+    @PostMapping("/verify")
+    @Operation(summary = "authenticate user",description = "Authenticate user by password and document")
+    public Boolean verifyAccessToken(@RequestParam(value = "access_token")String accessToken) throws JMBRException {
+        return Boolean.TRUE;
     }
 }
