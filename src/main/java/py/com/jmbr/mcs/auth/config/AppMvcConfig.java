fdb14195836d186ba6e7a964b61d81d359791564
juan <marcecinco@fpuna.edu.py>
2023-12-14 21:16:45 -0300

    Se elimina configuracion

--- src/main/java/py/com/jmbr/mcs/auth/config/AppMvcConfig.java
+++ src/main/java/py/com/jmbr/mcs/auth/config/AppMvcConfig.java
@@ -1,26 +1,17 @@
 package py.com.jmbr.mcs.auth.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import py.com.jmbr.java.commons.context.RequestContextInitializer;
 
 @Configuration
 @EnableWebMvc
 public class AppMvcConfig implements WebMvcConfigurer {
 
-
     @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new RequestContextInitializer());
-        WebMvcConfigurer.super.addInterceptors(registry);
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
-
-
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("/","/swagger-ui.html");
